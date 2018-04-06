c8b441cef92e1cfb507e4bd7b3b753a4fdb4b361
Carlos Sell <csell@outlook.com>
2018-04-06 09:52:12 -0600

    With notes for machine learning

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 REM From Jupyter Console
 REM Activate the virtual environment
-activate dstech-py36
+activate dsref
 REM Install it for Jupyter
-ipython kernel install --user --name=dstech-py36
+ipython kernel install --user --name=dsref
 Jupyter notebook
